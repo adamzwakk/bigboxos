2ff808a0d2c258d485a68a011bb3e5ed64113289
adamzwakk <adam@adamzwakk.com>
2025-09-07 21:35:53 -0400

    wolf3d cleanup

--- tools/convert_gog_to_cart.sh
+++ tools/convert_gog_to_cart.sh
@@ -2,30 +2,24 @@ innoextract -s --extract ./src/*.exe -d ./src/extracted/
 mkdir -p ./dist/package/files
 
 GAME_NAME=""
+SHORT_NAME="cart"
 GOG_INFO_FILE=$(find "./src/extracted" -maxdepth 1 -type f -name 'goggame*.info' | head -n 1 | tr -d '\r\n')
 if [ -f "$GOG_INFO_FILE" ]; then
     GAME_NAME=$(cat $GOG_INFO_FILE | jq '.name' | tr -d '"')
 fi
 
-# Wolf3D GOG
 if [ "$GAME_NAME" = "Wolfenstein 3D" ]; then
-    
     cp ./src/extracted/*.WL6 ./dist/package/files/
-
-    rm -r ./src/extracted/*
-
-    cat << EOF > ./dist/package/cart.ini
-[game]
-program=ecwolf
-cmd=cd /mnt/cart && ecwolf
-args=
-EOF
+    cp ./configs/wolf3d.ini ./dist/package/cart.ini
+    SHORT_NAME="wolf3d"
 fi
 
 (
     cd ./dist/package
-    zip -rqo ../cart.zip ./*
+    zip -rqo ../$SHORT_NAME.zip ./*
 )
+
+rm -r ./src/extracted/*
 rm -r ./dist/package/*
 
-echo "cart.zip made in ./dist/"
+echo "$SHORT_NAME.zip made in ./dist/"
